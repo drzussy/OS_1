c1f0a30545c98ef6d134b72f86a3b7eb6c5b3f40
Dan Nemirovski <dan.nemirovski@mail.huji.ac.il>
2025-04-07 15:37:05 +0300

    Added Page table eviction threshold

--- Resources/plot_example.py
+++ Resources/plot_example.py
@@ -5,19 +5,28 @@ import matplotlib.pyplot as plt
 data = pd.read_csv(r"/cs/usr/danemirovski/Documents/OS/ex1/OS_1/Resources/output4.csv")
 data = data.to_numpy()
 
-l1_size = 32000
-l2_size = 250000
-l3_size = 9000000
+l1_size = 32*1024
+l2_size = 250*1024  
+l3_size = 9*1024*1024 
+
+page_table_eviction_threshold = 2415919104 
 
 plt.plot(data[:, 0], data[:, 1], label="Random access")
 plt.plot(data[:, 0], data[:, 2], label="Sequential access")
 plt.xscale('log')
 plt.yscale('log')
+
+# Add vertical lines for cache sizes
 plt.axvline(x=l1_size, label="L1 (32 KiB)", c='r')
-plt.axvline(x=l2_size, label="L2 (250 KiB)", c='g')
-plt.axvline(x=l3_size, label="L3 (9 MiB)", c='brown')
+plt.axvline(x=l2_size, label="L2 (250 KiB)", c='g') 
+plt.axvline(x=l3_size, label="L3 (9 MiB)", c='brown') 
+
+# Add vertical line for Page Table Eviction Threshold
+plt.axvline(x=page_table_eviction_threshold, label="Page Table Eviction Threshold (2.25 GiB)", c='purple') 
+
 plt.legend()
 plt.title("Latency as a function of array size")
 plt.ylabel("Latency (ns log scale)")
 plt.xlabel("Bytes allocated (log scale)")
+plt.savefig("graph3.png",dpi=300)
 plt.show()
